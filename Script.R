99dd021edbf781c1c8ebf07865aa943f6b262bd6
195170-MuhammadOwais <142792713+MuhammadOwais766@users.noreply.github.com>
2025-04-05 18:20:08 +0300

    Final_Commit(Case_Study)

--- Script.R
+++ Script.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(nycflights13)
+
 glimpse(diamonds)
 
 ggplot(diamonds) + geom_bar(mapping = aes(x = cut))
@@ -96,3 +96,91 @@ nycflights13::flights %>% mutate(cancelled = is.na(dep_time),
 
 ggplot(data = diamonds, mapping = aes(x = price)) +
   geom_freqpoly(mapping = aes(colour = cut), binwidth = 500)
+
+ggplot(diamonds) + geom_bar(mapping = aes(x = cut))
+
+# Giving the graph a density of one
+
+ggplot(data = diamonds, mapping = aes(x = price, y = ..density..)) +
+  geom_freqpoly(mapping = aes(colour = cut), binwidth = 500)
+
+ggplot(data = diamonds, mapping = aes(x = cut, y = price)) + geom_boxplot()
+
+ggplot(data = mpg, mapping = aes(x = class, y = hwy)) + geom_boxplot()
+
+# Using 'reorder' to make more sense of the data
+
+ggplot(data = mpg) + geom_boxplot(mapping = aes(x = reorder(class, hwy, FUN = median),
+                                                y= hwy)) + coord_flip()
+
+
+# Categorical vs Categorical Variables
+
+ggplot(data = diamonds) + geom_count(mapping = aes(x = cut, y = color))
+
+diamonds %>% count(color, cut)
+
+# Using a simple heatmap to view the variation
+
+diamonds %>% count(color, cut) %>% ggplot(mapping = aes(x = color, y = cut)) +
+  geom_tile(mapping = aes(fill = n))
+
+# Continuous vs Continuous Variables
+
+# Lots of values in the visualization below
+
+ggplot(data = diamonds) + geom_point(mapping = aes(x = carat, y = price))
+
+# So use 'alpha = 1/100' instead for large datasets
+
+ggplot(data = diamonds) + geom_point(mapping = aes(x = carat, y = price), alpha = 1/100)
+
+library(hexbin)
+
+ggplot(data = diamonds2) + geom_hex(mapping = aes(x = carat, y= price))
+
+ggplot(data = diamonds2) + geom_bin2d(mapping = aes(x = carat, y= price))
+
+# We do the following visualization because while technically the data is 
+# continuous, by binning we're turning the data into categorical.
+
+#ggplot(data = diamonds, mapping = aes(x= carat, y = price)) + 
+#  geom_boxplot(mapping = aes(group = cut_width(carat, 0.1)))
+
+ggplot(data = small, mapping = aes(x= carat, y = price)) + 
+  geom_boxplot(mapping = aes(group = cut_width(carat, 0.1)))
+
+# Using 'cut_number(carat, 20)' to grab 20 at a time
+
+ggplot(data = small, mapping = aes(x= carat, y = price)) + 
+  geom_boxplot(mapping = aes(group = cut_number(carat, 20)))
+
+# Patterns and Models
+
+# Patterns in our data provide clues about the relationship between variables.
+# If a systematic relationship exists between two variables, it will appear as a
+# pattern in the data. Now ask the question whether this pattern is coincidental or
+# random chance. How can we describe the relationship implied by the pattern?
+# Is it positive or negative relationship? Linear or non-linear? Let's look at
+# the faithful data to find out.
+
+ggplot(data = faithful) + geom_point(mapping = aes(x = eruptions, y = waiting))
+
+# This package is used to do some linear regression
+
+library(modelr)
+
+mod <- lm(log(price) ~ log(carat), data = diamonds)
+
+diamonds2 <- diamonds %>% add_residuals(mod) %>% mutate(resid = exp(resid))
+?exp()
+
+ggplot(data = diamonds2) + geom_point(mapping = aes(x = carat, y= resid))
+
+ggplot(data = diamonds2) + geom_point(mapping = aes(x = carat, y= resid), alpha = 1/50)
+
+# The following plot reinforce what's been done above. It takes the previous
+# variation, and using the boxplot, makes the relationship between residuals and cut
+# even.
+
+ggplot(data = diamonds2) + geom_boxplot(mapping = aes(x = cut, y = resid))
